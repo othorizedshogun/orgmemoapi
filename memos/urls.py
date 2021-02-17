40b6fbbe451985a90bc334369d2182f5d0ef464b
boy <tommypinsel@gmail.com>
2021-02-17 12:10:42 +0100

    Created urlpath for MemoDetailAPIView

--- memos/urls.py
+++ memos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MemoListAPIView
+from .views import MemoListAPIView, MemoDetailAPIView
 
 
 
 urlpatterns = [
-    path('memos/', MemoListAPIView.as_view())
+    path('memos/', MemoListAPIView.as_view()),
+    path('memos/detail/<str:slug>/', MemoDetailAPIView.as_view())
 ]
 
