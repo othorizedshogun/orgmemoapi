2670fd0b10d7cbcf8f5d4cda08343d44496ccde2
boy <tommypinsel@gmail.com>
2021-02-17 11:29:29 +0100

    Added serializers.py

---
+++ memos/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Memo
+
+
+
+class MemoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Memo
+        fields = '__all__'
+
